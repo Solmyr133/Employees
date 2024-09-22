79e3d4bb8d05ffeed7a637c20f3e7453d3b71fa6
Solmyr77 <solmyr200@gmail.com>
2024-09-22 17:41:38 +0200

    Created class structure

---
+++ Employees/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Employees
+{
+    class Employee {
+        private int id;
+        private string name;
+        private int age;
+        private int salary;
+
+        public Employee(int id, string name, int age, int salary)
+        {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+            this.salary = salary;
+        }
+    }
+    internal class Program
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
